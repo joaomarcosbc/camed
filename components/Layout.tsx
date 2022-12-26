dda0dde31f7f534a28ab9abeda124d773d0de208
João Marcos <joaombcerqueira@gmail.com>
2022-12-25 23:46:56 -0300

    fix: layout positions

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Header.module.css";
+import styles from "../styles/Layouts.module.css";
 import Header from "../components/Header";
-import { Children } from "react";
 
 interface ILayoutProps {
   children: React.ReactNode;
@@ -12,7 +11,7 @@ export default function Layout({ children }: ILayoutProps) {
   return (
     <div className={styles.container}>
       <Header />
-      {children}
+      <div className={styles.childrenDiv}>{children}</div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
-import styles from "../styles/Header.module.css";
+import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import Image from "next/image";
+import equipe from "../public/equipe.png";
 
 export default function Home() {
   return (
@@ -14,7 +16,12 @@ export default function Home() {
         <link rel="icon" href="/logo.png" />
       </Head>
       <Layout>
-        <main className={styles.main}></main>
+        <div className={styles.container}>
+          <aside className={styles.aside}>
+            <div className={styles.image}></div>
+          </aside>
+          <main className={styles.main}></main>
+        </div>
       </Layout>
     </div>
   );
