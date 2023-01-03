36cc4fd2a618b7f5d162be2693fbcba24748e911
João Marcos <joaombcerqueira@gmail.com>
2023-01-02 23:37:00 -0300

    feat: text part and respondive design

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,32 @@ export default function Home() {
           <aside className={styles.aside}>
             <div className={styles.image}></div>
           </aside>
-          <main className={styles.main}></main>
+          <main className={styles.main}>
+            <div className={styles.board}>
+              <h1>QUEM SOMOS?</h1>
+              <p className={styles.text}>
+                Já nos conhece? Se não, muito prazer!
+                <br /> Nós somos o CAMED – Centro Acadêmico de Medicina “Dr.
+                Augusto César Leite” da Universidade Federal de Sergipe (UFS).
+                Nossos integrantes são alunos escolhidos por todos os estudantes
+                de medicina para que possamos representá-los e estabelecer uma
+                comunicação entre eles e as instâncias responsáveis pelo curso.
+                Assim, nosso trabalho gira em torno de fortalecer o movimento
+                estudantil, garantindo o direito dos acadêmicos de medicina e
+                lutar por uma educação médica de qualidade e cada vez mais
+                completa. Além disso, também atuamos promovendo eventos
+                esportivos, festas e outras atividades com o intuito de gerar
+                uma maior integração entre os alunos de diversos períodos. Nós
+                nos reunimos periodicamente e, além de debatermos os problemas
+                burocráticos, também temos uma ouvidoria para que vocês nos
+                tragam informações, críticas e sugestões para que possamos
+                melhorar, juntos, a experiência na universidade e que tenhamos
+                ótimos anos de formação.
+                <br /> Qualquer dúvida, é só entrar em contato com qualquer um
+                dos integrantes dessa gestão!
+              </p>
+            </div>
+          </main>
         </div>
       </Layout>
     </div>
